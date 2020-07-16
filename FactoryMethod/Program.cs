cc876b74c51bdeb6e333d196cb29ba0979178ca6
Mehmet Ziya Sürücü <mehmetziyasurucu@outlook.com>
2020-07-16 16:10:18 +0300

    Add project files.

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager=new CustomerManager();
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    public class LoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new EdLogger();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class EdLogger : ILogger
+    {
+        public void Log()
+        {
+           Console.WriteLine("Logged With EdLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            ILogger logger = new LoggerFactory().CreateLogger();
+            logger.Log();
+        }
+    }
+}
