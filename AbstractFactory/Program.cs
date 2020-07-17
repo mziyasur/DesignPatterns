8fde2cec635a1f1886c475ab54fd50adeceb1a3d
Mehmet Ziya Sürücü <mehmetziyasurucu@outlook.com>
2020-07-17 14:10:06 +0300

    AbstractFactory and Prototype Desİgn Patterns added to the project.

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager=new ProductManager(new Factory2());
+            productManager.GetAll();
+            Console.ReadLine();
+
+        }
+    }
+
+    public abstract class Logging
+    {
+        public abstract void Log(string message);
+    }
+
+    public class Log4NetLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with log4net");
+        }
+    }
+
+    public class NLogger:Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with nLogger");
+        }
+    }
+
+    public abstract class Caching
+    {
+        public abstract void Cache(string data);
+
+    }
+
+    public class MemCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with MemCache");
+        }
+    }
+
+    public class RedisCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with RedisCache");
+        }
+    }
+
+    public abstract class CrossCuttingConcernsFactorty
+    {
+        public abstract Logging CreateLogger();
+        public abstract Caching CreateCaching();
+    }
+
+    public class Factory1 : CrossCuttingConcernsFactorty
+    {
+        public override Caching CreateCaching()
+        {
+            return new RedisCache();
+            
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new Log4NetLogger();
+        }
+    }
+
+    public class Factory2 : CrossCuttingConcernsFactorty
+    {
+        public override Caching CreateCaching()
+        {
+            return new RedisCache();
+
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new NLogger();
+        }
+    }
+    public class ProductManager
+    {
+        private CrossCuttingConcernsFactorty _crossCuttingConcernsFactorty;
+        private Logging _logging;
+        private Caching _caching;
+
+        public ProductManager(CrossCuttingConcernsFactorty crossCuttingConcernsFactorty)
+        {
+            _crossCuttingConcernsFactorty = crossCuttingConcernsFactorty;
+            _logging = _crossCuttingConcernsFactorty.CreateLogger();
+            _caching = _crossCuttingConcernsFactorty.CreateCaching();
+        }
+
+        public void GetAll()
+        {
+            _logging.Log("Logged");
+            _caching.Cache("Data");
+            Console.WriteLine("Products Listed!");
+        }
+
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person.Customer customer1 = new Person.Customer
+                {FirstName = "Mehmet",
+                    LastName = "SÜRÜCÜ",
+                    City = "İstanbul",
+                    Id = 1};
+            
+            var customer2 = (Person.Customer)customer1.Clone();
+            customer2.FirstName = "Ziya";
+
+            Console.WriteLine(customer1.FirstName);
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();//prototip hale geldi.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public class Customer : Person
+        {
+            public string City { get; set; }
+
+            public override Person Clone()//Customeri Clonlamayı sağlar.
+            {
+                return (Person) MemberwiseClone();
+            }
+        }
+
+        public class Employee : Person
+        {
+
+            public decimal Salary { get; set; }
+            public override Person Clone()
+            {
+                return (Person)MemberwiseClone();
+            }
+        }
+
+    }
+
+    }
+
